6b8ae4afb5fc4f6c0e394b56fc6760873cbdcfc5
Yash Pawar <yashcpawar1410@gmail.com>
2023-01-21 22:30:52 +0530

    java notes

--- src/RajshriPractice/Variables.java
+++ src/RajshriPractice/Variables.java
@@ -1,14 +1,7 @@
 package RajshriPractice;
 
 public class Variables {
-    int a=10; // instance variable
-    static int b=20; // static variable
-
     public static void main(String[] args) {
-        int c = 30; // local variable
-        Variables a=new Variables();
-        System.out.println(a.a);
-        System.out.println(Variables.b);
-        System.out.println(c);
+
     }
 }
